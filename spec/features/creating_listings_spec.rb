cc53458df7d8bc62520535ca60acf2d2f5df0f77
Adam Butterfield <adb707@yahoo.com>
2014-06-17 13:41:41 -0700

    Refactor spec for necessary association between category and subcategory

--- spec/features/creating_listings_spec.rb
+++ spec/features/creating_listings_spec.rb
@@ -4,8 +4,8 @@ feature 'Creating Listings' do
   let!(:user) { FactoryGirl.create(:user) }
   before do
     sign_in_as!(user)
-    FactoryGirl.create(:category, name: 'Trees')
-    FactoryGirl.create(:subcategory, name: 'Coniferous')
+    trees = FactoryGirl.create(:category, name: 'Trees')
+    FactoryGirl.create(:subcategory, category: trees, name: 'Coniferous')
   end
 
   scenario "can create a listing" do
@@ -15,8 +15,8 @@ feature 'Creating Listings' do
     fill_in 'Title', with: 'Black Pine'
     fill_in 'Description', with: 'It\'s a black pine'
     fill_in 'Price', with: 9.99
-    choose 'Trees'
-    choose 'Coniferous'
+    choose 'listing[category_id]'
+    choose 'listing[subcategory_id]'
     check 'Willing to ship?'
     check 'Publish now?'
 
