9df7512e21185b2e121bf8ac7f23a1d0485f0ed1
Adam Butterfield <adb707@yahoo.com>
2014-06-18 16:00:42 -0700

    Implement ajax filter for listings by category and subcategory for marketplace

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -1,6 +1,20 @@
 class MarketplaceController < ApplicationController
   def index
-    @listings = Listing.postable
+    set_listings
     @categories = Category.includes(:subcategories).order(id: :asc)
   end
+
+  def filter_listings
+    if params[:category_id] or params[:subcategory_id]
+      @listings = Listing.postable.where('category_id = ? OR subcategory_id = ?', params[:category_id], params[:subcategory_id])
+    else
+      set_listings
+    end
+  end
+
+  private
+
+    def set_listings
+      @listings = Listing.postable
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'marketplace' => 'marketplace#index', as: 'marketplace'
-
+  get 'marketplace/filter_listings' => 'marketplace#filter_listings', as: 'filter_listings'
+  # get 'marketplace/:category(/:subcategory)' => 'marketplace/index', as: 'marketplace_filter'
   get 'listings/set_subcategories' => 'listings#set_subcategories', as: 'set_subcategories'
   resources :listings
   # get '/:category/:id' => 'listings#show' as: 'listing'
