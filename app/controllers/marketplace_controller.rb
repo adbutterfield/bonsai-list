5579c2b42df1b38e9c234d27c30b2f83beb425eb
Adam Butterfield <adb707@yahoo.com>
2014-06-25 15:45:56 -0700

    Dry up controller

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -1,17 +1,20 @@
 class MarketplaceController < ApplicationController
+  before_action :set_listings, only: [:index, :ajax_sort]
   def index
     @categories = Category.includes(:subcategories).order(id: :asc)
-    @listings = Listing.filter_listings(params, user_postcode)
   end
 
   def ajax_sort
-    @listings = Listing.filter_listings(params, user_postcode)
     respond_to do |format|
       format.js
     end
   end
 
   private
+
+    def set_listings
+      @listings = Listing.filter_listings(params, user_postcode)
+    end
 
     def user_postcode
       current_user.postcode
