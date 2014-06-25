e204c468926c37c29c71acb256fef93ca9922462
Adam Butterfield <adb707@yahoo.com>
2014-06-25 10:54:17 -0700

    Clean up controllers

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,68 +3,49 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy, :remove]
   before_action :set_location, only: :create
 
-
-  # GET /listings
-  # GET /listings.json
   def index
     @listings = Listing.all
   end
 
-  # GET /listings/1
-  # GET /listings/1.json
   def show
   end
 
-  # GET /listings/new
   def new
     @listing = current_user.listings.build
     @categories = Category.all
     @subcategories = Subcategory.where('category_id =?', Category.first).order(id: :asc)
   end
 
-  # GET /listings/1/edit
   def edit
     @categories = Category.all
     @subcategories = Subcategory.where(category_id: @listing.category_id).order(id: :asc)
   end
 
-  # POST /listings
-  # POST /listings.json
   def create
     @listing = current_user.listings.build(listing_params)
-    # raise
     respond_to do |format|
       if @listing.save
         format.html { redirect_to user_root_url, notice: 'Listing was successfully created.' }
-        # format.json { render :show, status: :created, location: @listing }
       else
         format.html { render :new }
-        # format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /listings/1
-  # PATCH/PUT /listings/1.json
   def update
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
       else
         format.html { render :edit }
-        format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /listings/1
-  # DELETE /listings/1.json
   def destroy
     @listing.destroy
     respond_to do |format|
       format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -81,12 +62,10 @@ class ListingsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
       params.require(:listing).permit(:title, :description, :price, :shippable, :publish, :location, :remove, :user_id, :category_id, :subcategory_id)
     end
