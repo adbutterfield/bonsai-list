cc22581d4d399e397e603937b41d9f3fa16ae331
Adam Butterfield <adb707@yahoo.com>
2014-09-08 15:30:49 -0700

    Set variables to different address parts

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def new
-    @address = Geocoder.search(session[:coordinates])[0].formatted_address
+    address = Geocoder.search(cookies[:location])[0]
+    # street_number  = @address.address_components[0]['long_name']
+    # street_name  =  @address.address_components[1]['long_name']
+    @city_name  = address.address_components[2]['long_name']
+    @state_name = address.address_components[4]['long_name']
+    @country_name = address.address_components[5]['long_name']
+    @post_code = address.address_components[6]['long_name']
+    @address = "#{city_name}, #{state_name}, #{@post_code}, #{country_name}"
     super
   end
 end
