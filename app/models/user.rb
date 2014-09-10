a9459ff1e092b71168c0b8fcef4b837e48191096
Adam Butterfield <adb707@yahoo.com>
2014-09-09 22:21:31 -0700

    Remove postcode from presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
+  has_one :address
   has_many :listings, -> { where(remove: false) }
   has_many :posted_listings, -> { where(remove: false, publish: true) }, class_name: 'Listing'
+
+  accepts_nested_attributes_for :address
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :firstname, :lastname, :postcode, presence: true
+  validates :firstname, :lastname, presence: true
 
 end
