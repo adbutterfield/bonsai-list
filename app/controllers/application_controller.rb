cdf014d05bb5d4a3bbe9ab75d7861bb1440d2847
Adam Butterfield <adb707@yahoo.com>
2014-09-09 23:50:04 -0700

    Add after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   # after_sign_out_path_for TODO
 
+  def after_sign_in_path_for(resource)
+    user_root_path
+  end
+
   protected
 
   def configure_permitted_parameters
