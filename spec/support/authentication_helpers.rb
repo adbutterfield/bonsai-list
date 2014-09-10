23d53e2a6761abecc5b3cc38624c179670d90654
Adam Butterfield <adb707@yahoo.com>
2014-09-09 23:57:04 -0700

    Change _url to _path for helpers

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,9 +1,10 @@
 module AuthenticationHelpers
   def sign_in_as!(user)
     visit new_user_session_path
+
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button 'Sign In'
-    expect(page.current_url).to eql(user_root_url)
+    expect(page.current_path).to eql(user_root_path)
   end
 end
