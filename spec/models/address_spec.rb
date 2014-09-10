89b4865a1a6de0b310f90871fb7011665689345b
Adam Butterfield <adb707@yahoo.com>
2014-09-10 12:57:59 -0700

    Add tests for factory and validations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Address, :type => :model do
+  subject(:address) { FactoryGirl.create(:address) }
 
+  it "should have a valid factory" do
+    expect(address).to be_valid
+  end
+
+  describe "Validations" do
+    [:city, :state, :postcode, :country].each do |attr|
+      it "is invalid without a #{attr}" do
+        expect(FactoryGirl.build(:address, attr => nil)).to_not be_valid
+      end
+    end
+  end
 end
