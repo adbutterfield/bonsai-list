8650ebaa15346e14170b50d64dcdc5541539862b
Adam Butterfield <adb707@yahoo.com>
2014-09-10 12:53:42 -0700

    Add validates assocated for address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   has_one :address
+  validates_associated :address
   has_many :listings, -> { where(remove: false) }
   has_many :posted_listings, -> { where(remove: false, publish: true) }, class_name: 'Listing'
 
-  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :address, allow_destroy: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
