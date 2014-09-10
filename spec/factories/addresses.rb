1541708738838094a67f63f07bb2520ae123ba48
Adam Butterfield <adb707@yahoo.com>
2014-09-10 12:55:41 -0700

    Use faker for data

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,11 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :address do
-    city "MyString"
-    state "MyString"
-    postcode "MyString"
-    country "MyString"
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    postcode { Faker::Address.postcode }
+    country { Faker::Address.country }
     user nil
   end
 end
