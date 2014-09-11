178246a77a31a8c5aeac903cc48329d02ec04477
Adam Butterfield <adb707@yahoo.com>
2014-09-11 10:26:16 -0700

    Geocode address after validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,11 @@ class Address < ActiveRecord::Base
   belongs_to :user, inverse_of: :address
 
   validates :city, :state, :postcode, :country, presence: true
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    [self.city, self.state, self.postcode, self.country].join(', ')
+  end
 end
