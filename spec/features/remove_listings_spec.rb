7bc9c9628e26f808d7fe510ee23968267b669ef0
Adam Butterfield <adb707@yahoo.com>
2014-09-10 20:11:39 -0700

    Add scenario for canceling listing removal

--- spec/features/remove_listings_spec.rb
+++ spec/features/remove_listings_spec.rb
@@ -22,6 +22,18 @@ feature 'Removing Listings' do
     expect(page.current_path).to eql(user_root_path)
     expect(page).to have_content('Listing was successfully removed.')
     expect(page).not_to have_content(@listing.title)
+  end
 
+
+  @javascript
+  scenario "can remove a listing" do
+    visit user_root_path
+    expect(page).to have_content(@listing.title)
+
+    click_link "Remove"
+    page.driver.browser.switch_to.alert.dismiss
+    expect(page.current_path).to eql(user_root_path)
+
+    expect(page).to have_content(@listing.title)
   end
 end
