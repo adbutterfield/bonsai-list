00e01aaa0853da5e7db38d805f804e70f2a2a00f
Adam Butterfield <adb707@yahoo.com>
2014-09-11 10:18:10 -0700

    Add lat and lon to address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908224146) do
+ActiveRecord::Schema.define(version: 20140911045438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20140908224146) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "latitude"
+    t.string   "longitude"
   end
 
   add_index "addresses", ["user_id"], name: "index_addresses_on_user_id", using: :btree
