b1392399bb415aa863bc1174ea653c384d4ec522
Adam Butterfield <adb707@yahoo.com>
2014-09-12 11:36:54 -0700

    Add validation tests for latitude and longitude, add test for set_latitude_and_longitude

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -9,10 +9,24 @@ RSpec.describe Listing, :type => :model do
   end
 
   describe "validations" do
-    [:title, :description, :price, :category, :subcategory]. each do |attr|
+    [:title, :description, :price, :category, :subcategory, :longitude, :latitude, :user_id]. each do |attr|
       it "is invalid without a #{attr}" do
         expect(FactoryGirl.build(:listing, attr => nil)).not_to be_valid
       end
     end
   end
+
+  describe "#set_latitude_and_longitude" do
+    it "should set latitude and longitude of the current_user" do
+      user = FactoryGirl.create(:user_with_address)
+      listing = FactoryGirl.build(:listing)
+      listing.set_latitude_and_longitude(user)
+      listing.save
+      # currently, faker makes lat and lon strings, an open pull request may soon fix that
+      # https://github.com/stympy/faker/pull/246
+      expect(listing.longitude).to eq(user.address.longitude.to_f)
+      expect(listing.latitude).to eq(user.address.latitude.to_f)
+    end
+  end
+
 end
