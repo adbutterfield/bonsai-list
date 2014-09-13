0f5c960a0843d718b10b1d21509a9454ca2fbeb7
Adam Butterfield <adb707@yahoo.com>
2014-09-13 00:05:14 -0700

    Add basic password protection to site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with name: "bonchan", password: "bonbon"
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
