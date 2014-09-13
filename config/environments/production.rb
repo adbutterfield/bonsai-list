1159dcdf8d558bf667849ace0ae41f166305ca2e
Adam Butterfield <adb707@yahoo.com>
2014-09-12 21:24:19 -0700

    Remove join for font path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( .svg .eot .woff .ttf)
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  # config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
