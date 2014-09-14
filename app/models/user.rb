1a6e8394f415f36e05c249fd3906f972feefd969
Adam Butterfield <adb707@yahoo.com>
2014-09-13 23:05:25 -0700

    Validate uniqueness for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :firstname, :lastname, presence: true
+  validates :email, uniqueness: true
 
 end
