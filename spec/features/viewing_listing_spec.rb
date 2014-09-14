40c2c8be20b0992f5712245fb0a4465f672ca0f7
Adam Butterfield <adb707@yahoo.com>
2014-09-13 22:39:21 -0700

    Add feature spec for viewing a listing

---
+++ spec/features/viewing_listing_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+# TODO test basically duplicated from creating listing spec, need to refactor to view spec
+feature 'Viewing Listing' do
+  let!(:user) { FactoryGirl.create(:user_with_address) }
+  before do
+    sign_in_as!(user)
+    trees = FactoryGirl.create(:category, name: 'Trees')
+    FactoryGirl.create(:subcategory, category: trees, name: 'Coniferous')
+  end
+
+  scenario "can create a listing" do
+
+    click_link "Add new listing"
+
+    expect(page.current_path).to eql(new_listing_path)
+
+    fill_in 'Title', with: 'Black Pine'
+    fill_in 'Description', with: 'It\'s a black pine'
+    fill_in 'Price', with: 9.99
+    # Capybara cannot find the 'Trees' radio button, but it's chosen by default.
+    # choose 'Trees'
+    choose 'Coniferous'
+    check 'Willing to ship?'
+    check 'Publish now?'
+
+    click_button 'Save Listing'
+
+    listing = Listing.last
+
+    expect(listing.title).to eq('Black Pine')
+    expect(listing.description).to eq('It\'s a black pine')
+    expect(listing.price).to eq(9.99)
+
+    expect(page.current_path).to eql(user_root_path)
+
+    expect(page).to have_content('Listing was successfully created.')
+    expect(page).to have_content(listing.title)
+
+    click_link listing.title
+
+    expect(page).to have_content(listing.title)
+    expect(page).to have_content(listing.description)
+    expect(page).to have_content(listing.price)
+    expect(page).to have_content(user.address.city)
+    expect(page).to have_content(user.address.state)
+    expect(page).to have_content("Willing to ship")
+
+
+  end
+end
