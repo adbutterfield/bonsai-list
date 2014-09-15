fc1b0783967bba50de35a16b5fd53714bf3a3860
Adam Butterfield <adb707@yahoo.com>
2014-09-14 21:56:24 -0700

    Add spec for marketplace index

---
+++ spec/views/marketplace/index.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "marketplace/index" do
+
+  before do
+    assign(:categories, FactoryGirl.create(:category, name: "tree"))
+  end
+
+  it "displays the no listings message if no listings" do
+    assign(:listings, [])
+    render :template => 'marketplace/index'
+    expect(rendered).to match /No listings found/
+  end
+
+  it "displays all the listings" do
+    assign(:listings, [
+      FactoryGirl.create(:listing, title: "pine"),
+      FactoryGirl.create(:listing, title: "pot")
+    ])
+
+    render
+
+    expect(rendered).to match /pine/
+    expect(rendered).to match /pot/
+  end
+end
