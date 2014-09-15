9aa04b918d56a86cecf3608b02865d9228fc960f
Adam Butterfield <adb707@yahoo.com>
2014-09-14 18:03:57 -0700

    Remove sessions controller, no longer need to reset location cookie

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  after_action :clear_location_cookie, only: :destroy
-
-  private
-    def clear_location_cookie
-      cookies[:location] = nil
-    end
-end
