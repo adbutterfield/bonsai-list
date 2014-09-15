ba85ac17e2eee9a39f02ec2d3e7738a26cfadffc
Adam Butterfield <adb707@yahoo.com>
2014-09-14 20:36:54 -0700

    Refactor registrations new to build address with attributes from location helper

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :set_address_variables, only: :new
 
   def new
-    super
+    build_resource({})
+    self.resource.address = Address.new(city: location.city, state: location.state, country: location.country, postcode: location.postal_code, latitude: location.latitude, longitude: location.longitude)
+    respond_with self.resource
   end
 
-  private
-    def set_address_variables
-      @city_name    = location.city
-      @state_name   = location.state
-      @country_name = location.country
-      @post_code    = location.postal_code
-      @latitude     = location.latitude
-      @longitude    = location.longitude
-    end
 end
