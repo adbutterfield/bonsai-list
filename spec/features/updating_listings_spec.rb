070a04268241a02b5aba1c4c748c3b6ae4b6a148
Adam Butterfield <adb707@yahoo.com>
2014-09-15 22:57:11 -0700

    Add scenario for when a user tries to edit another user's listing

--- spec/features/updating_listings_spec.rb
+++ spec/features/updating_listings_spec.rb
@@ -25,4 +25,15 @@ feature 'Updating Listings' do
     expect(listing.published_at).to_not be_nil
   end
 
+  scenario "doesn't allow editing another user's listing" do
+    other_user = FactoryGirl.create(:user_with_address)
+    other_user_listing = FactoryGirl.create(:listing, user: other_user)
+
+    visit edit_listing_path(other_user_listing)
+
+    expect(page.current_path).to eql(user_root_path)
+
+    expect(page).to have_content("We can't find the page you're looking for...")
+  end
+
 end
