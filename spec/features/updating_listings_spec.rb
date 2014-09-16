3330440b522bcc55cf786fe8f94fa906641ab349
Adam Butterfield <adb707@yahoo.com>
2014-09-15 22:56:33 -0700

    Add scenario for updating publish to true

--- spec/features/updating_listings_spec.rb
+++ spec/features/updating_listings_spec.rb
@@ -8,4 +8,21 @@ feature 'Updating Listings' do
     FactoryGirl.create(:subcategory, category: trees, name: 'Coniferous')
   end
 
+  scenario "sets published_at when updated to publish true" do
+    listing = FactoryGirl.create(:listing, user: user, publish: false)
+
+    expect(listing.published_at).to be_nil
+
+    visit edit_listing_path(listing)
+
+    check "Publish now?"
+
+    click_button "Save Listing"
+
+    expect(page).to have_content('Listing was successfully updated.')
+
+    listing = Listing.last
+    expect(listing.published_at).to_not be_nil
+  end
+
 end
