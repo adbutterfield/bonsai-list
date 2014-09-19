63e885f2334b85c7d458f91a8dcf5f5f7ad9cb95
Adam Butterfield <adb707@yahoo.com>
2014-09-19 16:03:37 -0700

    Add messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,34 @@
+class MessagesController < ApplicationController
+
+  def index
+    @new_messages = current_user.mailbox.inbox
+    raise
+    # **** this is the conversation ****
+    # gets the last conversation (chronologically, the first in the inbox)
+    # current_user.mailbox.inbox(unread: true).first
+
+    # here is how you get the subject
+    # current_user.mailbox.inbox(unread: true).first.subject
+
+    # **** this is the message, the collection of receipts ****
+    # current_user.mailbox.inbox(unread: true).first.messages
+
+    # get the sender
+    # current_user.mailbox.inbox(unread: true).first.messages.first.sender
+
+    # here's how you check whether the receipt is unread for the current_user
+    # current_user.mailbox.inbox(unread: true).first.messages.first.is_unread? current_user
+
+    # **** this is the collection of receipts for a particular user
+    # gets receipts chronologically ordered
+    # current_user.mailbox.inbox(unread: true).first.receipts_for(current_user)
+
+    # get the sender of the receipt
+    # current_user.mailbox.inbox(unread: true).first.receipts_for(current_user).first.sender
+
+    # check if a receipt is unread
+    # current_user.mailbox.inbox(unread: true).first.receipts_for(current_user).first.is_unread?
+
+  end
+
+end
