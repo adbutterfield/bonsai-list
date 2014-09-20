07b0ed5c5a550a04148cf30efd0ed3866ea6063b
Adam Butterfield <adb707@yahoo.com>
2014-09-19 23:16:57 -0700

    Add test for viewing a message

--- spec/features/viewing_listing_interest_messages.rb
+++ spec/features/viewing_listing_interest_messages.rb
@@ -2,15 +2,31 @@ require 'rails_helper'
 
 feature 'Viewing Messages of Interest for Listings' do
 
-  scenario "can view the messages of interest" do
-
     before do
       @user = FactoryGirl.create(:user_with_address)
-      sign_in @user
+      sign_in_as!(@user)
       @other_user = FactoryGirl.create(:user_with_address)
       @user.send_message(@other_user, "User message body", "User message subject")
       @other_user.send_message(@user, "Other user message body", "Other user message subject")
     end
+
+  scenario "can view the messages of interest" do
+      puts @user.mailbox.inbox.first.subject
+
+
+    visit user_root_path
+
+    within("#home-nav") do
+
+      click_link "Messages"
+
+    end
+
+    click_link "Other user message subject"
+
+    expect(page).to have_content("Other user message body")
+
+    expect(page).to_not have_content("User message body")
 
   end
 
