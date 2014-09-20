cf92607b20c298f8593964d3e396f5b8bb34543c
Adam Butterfield <adb707@yahoo.com>
2014-09-19 20:15:50 -0700

    Add feature spec for viewing messages of interest

--- spec/features/viewing_listing_interest_messages.rb
+++ spec/features/viewing_listing_interest_messages.rb
@@ -1,1 +1,17 @@
-viewing_listing_interest_messages.rb
+require 'rails_helper'
+
+feature 'Viewing Messages of Interest for Listings' do
+
+  scenario "can view the messages of interest" do
+
+    before do
+      @user = FactoryGirl.create(:user_with_address)
+      sign_in @user
+      @other_user = FactoryGirl.create(:user_with_address)
+      @user.send_message(@other_user, "User message body", "User message subject")
+      @other_user.send_message(@user, "Other user message body", "Other user message subject")
+    end
+
+  end
+
+end
