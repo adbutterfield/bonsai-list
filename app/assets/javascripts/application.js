a807586c5994563f19742097f412995c3e40b481
Adam Butterfield <adb707@yahoo.com>
2014-09-23 23:13:18 -0700

    Use sticky scroll for icon bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,22 @@
 //= require jquery_ujs
 //= require foundation
 //= require marketplace
-//= require jquery_cookie
+//= require sticky_scroll
 //= require_tree .
 
 $(function(){
 
   $(document).foundation();
+  var barHeight = ($( window ).height() - 85);
+  // var breakPoint =  $( document ).height() - $( window ).height() - 85
+  $("#mobile-icon-bar").sticky({ topSpacing: barHeight });
+
+  // $( document ).scroll(function (){
+  //   if ( $( document ).scrollTop() >= breakPoint ){
+  //     barHeight = $( window ).height() - 132 - ($( document ).scrollTop() - breakPoint)
+  //     $("#mobile-icon-bar").sticky({ topSpacing: barHeight });
+  //   }
+  //   // if
+  // });
 
 });
