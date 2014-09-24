fcd335b3692cd44a70f37c7e2130adff6cd97456
Adam Butterfield <adb707@yahoo.com>
2014-09-23 20:11:57 -0700

    Modify spec to account for modal

--- spec/features/inquiring_to_listing_spec.rb
+++ spec/features/inquiring_to_listing_spec.rb
@@ -5,17 +5,21 @@ feature 'Inquiring to Listings' do
     @user = FactoryGirl.create(:user_with_address)
     sign_in_as!(@user)
     @other_user = FactoryGirl.create(:user_with_address)
-    @listing = FactoryGirl.create(:listing, headline: "Black pine", user: @other_user)
+    @sale_listing = FactoryGirl.create(:listing, headline: "Black pine", user: @other_user, sale_type: "sale")
   end
 
-  scenario "can notify other user of being interested a listing" do
+  scenario "can notify other user of being interested a listing", js: true do
 
-    visit listing_path(@listing)
-    # TODO change to use modal
+    visit listing_path(@sale_listing)
+
     click_link "I'm interested!"
+    click_button "Send"
+
+    expect(page).to have_content("Inquiry sent!")
 
     expect(@other_user.mailbox.inbox(unread: true).count).to eq 1
     expect(@user.mailbox.sentbox.count).to eq 1
 
   end
+
 end
