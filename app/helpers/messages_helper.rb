08ef5ff207d73c2bd2da23566e0eb67cc047c7ab
Adam Butterfield <adb707@yahoo.com>
2014-09-24 23:01:01 -0700

    Add todo note

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,4 +1,5 @@
 module MessagesHelper
+  #TODO get rid of messages helper if it's not used
   def set_message_notice_text(conversation)
     if conversation.receipts_for(current_user).first.is_unread?
       return conversation.subject
