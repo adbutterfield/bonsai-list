8b621ebc427365459725b3f0a7c969d9828693a6
Adam Butterfield <adb707@yahoo.com>
2014-09-25 23:12:19 -0700

    Add inquiries controller to handle creating inquiry

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,22 @@
+class InquiriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    listing = Listing.find(params[:id])
+    current_user.send_message(listing.user, message_body, message_subject(listing))
+    conversation = Mailboxer::Conversation.last
+    Inquiry.create(listing_id: listing.id, user_id: current_user.id, offer: params[:offer], conversation_id: conversation.id)
+    redirect_to listing
+  end
+
+  private
+
+  def message_body
+    params[:body] || "Please let me know if it's still for sale!"
+  end
+
+  def message_subject(listing)
+    "#{current_user.full_name} is interested in your #{listing.headline}!"
+  end
+
+end
