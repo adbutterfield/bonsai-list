e4806da2e04e6fd752d4bfb2e5b2c8177fc2f5d0
Adam Butterfield <adb707@yahoo.com>
2014-09-26 20:03:17 -0700

    Move set up objects into before block

--- spec/views/listings/show.html.erb_spec.rb
+++ spec/views/listings/show.html.erb_spec.rb
@@ -2,18 +2,18 @@ require "rails_helper"
 
 RSpec.describe "listings/show" do
 
-  user = FactoryGirl.create(:user_with_address)
-  listing = FactoryGirl.create(:listing, user: user)
   before do
-    assign(:listing, listing)
+    user = FactoryGirl.create(:user_with_address)
+    @listing = FactoryGirl.create(:listing, user: user)
+    assign(:listing, @listing)
   end
 
   it "displays the listing" do
     render :template => 'listings/show'
 
-    expect(rendered).to have_content(listing.headline)
-    expect(rendered).to have_content(listing.description)
-    expect(rendered).to have_content(listing.price)
+    expect(rendered).to have_content(@listing.headline)
+    expect(rendered).to have_content(@listing.description)
+    expect(rendered).to have_content(@listing.price)
     expect(rendered).to have_content(user.full_name)
     expect(rendered).to have_content(user.address.city)
     expect(rendered).to have_content(user.address.state)
