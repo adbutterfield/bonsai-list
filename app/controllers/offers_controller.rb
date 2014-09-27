f56366b107888e9b0a0623af9d838517d4e96c43
Adam Butterfield <adb707@yahoo.com>
2014-09-26 21:27:20 -0700

    Add verify_user! before action

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,7 @@
 class OffersController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :verify_user!, only: :show
+
 
   def index
     # TODO fix n+1 query for getting inquiry count
@@ -9,5 +11,12 @@ class OffersController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
     @inquiries = @listing.offers
+  end
+
+  private
+
+  def verify_user!
+    user = Listing.find(params[:id]).user if params[:id]
+    redirect_to user_root_path, notice: "We can't find the page you're looking for..." unless current_user?(user)
   end
 end
