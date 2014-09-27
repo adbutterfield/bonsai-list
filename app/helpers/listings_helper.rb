33609d52b42b100ca2eb0a2d4e7f40b08fb0cd67
Adam Butterfield <adb707@yahoo.com>
2014-09-26 19:59:46 -0700

    Use Listing#is_offer? for not_already_inquired helper method

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -13,10 +13,10 @@ module ListingsHelper
         return link_to 'Edit listing', edit_listing_path(@listing), :class => "button"
       end
       if current_user.not_already_inquired?(@listing)
-        if @listing.sale_type == "sale"
-          return "<a class='button' href='#' data-reveal-id='interestedModal'>I'm interested!</a>".html_safe
-        else
+        if @listing.is_offer?
           return "<a class='button' href='#' data-reveal-id='interestedModal'>Make offer!</a>".html_safe
+        else
+          return "<a class='button' href='#' data-reveal-id='interestedModal'>I'm interested!</a>".html_safe
         end
       else
         return "<a class='button disabled'>Inquiry sent!</a>".html_safe
