bcec013a0465de2a7ae0f45d6fdbbb5317474aa2
Adam Butterfield <adb707@yahoo.com>
2014-09-26 19:58:35 -0700

    Get offers with class method on Listing

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,8 @@ class OffersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-
+    # TODO fix n+1 query for getting inquiry count
+    @offer_listings = Listing.inquired_on(current_user)
   end
 
   def show
