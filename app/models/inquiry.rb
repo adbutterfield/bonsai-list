15eb7de851053d9fb5d074044eeaf16bef9ef279
Adam Butterfield <adb707@yahoo.com>
2014-09-26 23:45:06 -0700

    Add scopes for not_seen and seen inquiries

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,6 +2,10 @@ class Inquiry < ActiveRecord::Base
   belongs_to :user
   belongs_to :listing
 
+  scope :not_seen, -> { where(is_seen: false) }
+  scope :seen, -> { where(is_seen: true) }
+
+
   def conversation
     Mailboxer::Conversation.find(self.conversation_id)
   end
