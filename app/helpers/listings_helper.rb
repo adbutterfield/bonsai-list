c9fb801a75bd0362e0272700123e31b81973e97b
Adam Butterfield <adb707@yahoo.com>
2014-09-28 21:55:45 -0700

    Remove willing_to_ship?

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,11 +1,4 @@
 module ListingsHelper
-  def willing_to_ship?(listing)
-    if listing.shippable
-      return "Willing to ship"
-    else
-      return "Not willing to ship"
-    end
-  end
 
   def pricing_table_button
     if user_signed_in?
