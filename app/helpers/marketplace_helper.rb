be8cb1bed819c0a4c22bc3dd3d9b2edfb0a2684a
Adam Butterfield <adb707@yahoo.com>
2014-09-29 19:17:58 -0700

    Add helper method to check if user marketplace page is for current user

--- app/helpers/marketplace_helper.rb
+++ app/helpers/marketplace_helper.rb
@@ -2,4 +2,8 @@ module MarketplaceHelper
   def no_listings_message
     "<p class='text-center'>No listings found</p>".html_safe
   end
+
+  def not_current_user(user)
+    current_user != user
+  end
 end
