d8e3e797132180bbe8cb583e62dfa5515a088b0c
Adam Butterfield <adb707@yahoo.com>
2014-09-30 22:47:40 -0700

    Remove comment

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,5 +1,4 @@
 module MessagesHelper
-  #TODO get rid of messages helper if it's not used
   def set_message_notice_text(conversation)
     if conversation.receipts_for(current_user).first.is_unread?
       return conversation.subject
