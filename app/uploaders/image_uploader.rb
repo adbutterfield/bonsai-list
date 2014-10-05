d4927a7a69ef65a9f425d46d4df76eff9652a5b3
Adam Butterfield <adb707@yahoo.com>
2014-10-04 21:29:57 -0700

    Add extension white list, and marketplace version

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,6 +16,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -34,6 +38,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fit => [70, 70]
+  end
+
+  version :marketplace do
+    process :resize_to_fill => [310, 310]
   end
 
   version :main do
