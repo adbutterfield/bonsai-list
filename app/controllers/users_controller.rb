5c782425e17245d3bd33114652045d7e9fd55f2f
Adam Butterfield <adb707@yahoo.com>
2014-10-05 14:05:22 -0700

    Sort listings by newest first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    @listings = current_user.listings
+    @listings = current_user.listings.order(created_at: :desc)
   end
 end
