b5717586eea128419d2e0b6438c8f0a09ee4fd60
Adam Butterfield <adb707@yahoo.com>
2014-10-07 12:25:37 -0700

    Remove no_listings_message helper

--- app/helpers/marketplace_helper.rb
+++ app/helpers/marketplace_helper.rb
@@ -1,8 +1,4 @@
 module MarketplaceHelper
-  def no_listings_message
-    "<p class='text-center'>No listings found</p>".html_safe
-  end
-
   def not_current_user(user)
     current_user != user
   end
