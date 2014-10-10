2de3807b56833989280e0d0f741f48203d9666a3
Adam Butterfield <adb707@yahoo.com>
2014-10-09 20:20:28 -0700

    Add timezome to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006021139) do
+ActiveRecord::Schema.define(version: 20141010030549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 20141006021139) do
     t.datetime "updated_at"
     t.string   "pic"
     t.string   "avatar"
+    t.string   "timezone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
