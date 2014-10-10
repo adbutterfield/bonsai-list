fcda2a57633c2923d78a6c41fa4bf948709edc42
Adam Butterfield <adb707@yahoo.com>
2014-10-09 20:20:53 -0700

    Add config for timezone gem

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module BonsaiList
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
   end
+end
+
+Timezone::Configure.begin do |c|
+  c.username = Figaro.env.GEONAMES
 end
