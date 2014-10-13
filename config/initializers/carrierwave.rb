ed1f30980da49c5f5689254541043109e61c1024
Adam Butterfield <adb707@yahoo.com>
2014-10-12 22:43:41 -0700

    Configre carrierwave to save files locally for testing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,17 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => Figaro.env.S3_KEY,
-    :aws_secret_access_key  => Figaro.env.S3_SECRET,
-    :region                 => Figaro.env.S3_REGION
-  }
-  config.fog_directory  = Figaro.env.S3_BUCKET
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Figaro.env.S3_KEY,
+      :aws_secret_access_key  => Figaro.env.S3_SECRET,
+      :region                 => Figaro.env.S3_REGION
+    }
+    config.fog_directory  = Figaro.env.S3_BUCKET
+  end
 end
