31e0ff9e099c723ee89875645b846f6f52eed676
Adam Butterfield <adb707@yahoo.com>
2014-10-12 22:47:57 -0700

    Set image to use test upload image

--- spec/factories/listing_images.rb
+++ spec/factories/listing_images.rb
@@ -1,8 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :listing_image do
     listing nil
-    image "MyString"
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'photos', 'test.png')) }
   end
 end
