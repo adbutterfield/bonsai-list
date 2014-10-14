366cdba53eacb7d1f3776d5efd69d496e7a6c5f4
Adam Butterfield <adb707@yahoo.com>
2014-10-14 10:20:23 -0700

    Add require for capybara-email

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara/email/rspec'
 require 'capybara/poltergeist'
 require 'database_cleaner'
 
