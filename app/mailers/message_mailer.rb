59fd2296fce7bd2588994ed4252975c0d6910689
Adam Butterfield <adb707@yahoo.com>
2014-10-13 23:25:13 -0700

    Add new_messge_email action

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,3 +1,10 @@
 class MessageMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@bonsai-list.com"
+
+  def new_message_email(conversation)
+    @conversation = conversation
+    @sender = conversation.participants.first
+    @recipient = conversation.participants.last
+    mail(to: @recipient.email, subject: "You received a new message | Bonsai List")
+  end
 end
