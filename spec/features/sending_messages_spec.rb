33f910572a9ef51fa22bef3186c682a7ebab90db
Adam Butterfield <adb707@yahoo.com>
2014-10-13 22:22:49 -0700

    Add scenarios for sending and replying to messages

--- spec/features/sending_messages_spec.rb
+++ spec/features/sending_messages_spec.rb
@@ -4,15 +4,45 @@ feature "Sending messages" do
 
   before do
     @user = FactoryGirl.create(:user)
-    sign_in_as!(@user)
     @other_user = FactoryGirl.create(:user)
-    @user.send_message(@other_user, "User message body", "User message subject")
-    conversation = Mailboxer::Conversation.first
-    @other_user.reply_to_conversation(conversation, "Here is the reply")
   end
 
-  scenario "sending an initial message" do
+  scenario "sending a message from a users show page", js: true do
+    sign_in_as!(@user)
+
+    visit user_marketplace_path(@other_user)
+
+    click_link "Send Message"
+
+    fill_in "Subject", with: "I send you a message"
+    fill_in "Message", with: "Here is the message"
+
+    click_button "Send"
+
     visit sent_path
-    expect(page).to have_content("User message subject")
+    expect(page).to have_content("I send you a message")
+    click_link "Sign Out"
+
+    sign_in_as!(@other_user)
+    visit messages_path
+    expect(page).to have_content("I send you a message")
+    click_link "Sign Out"
+  end
+
+  scenario "replying to a message" do
+    @user.send_message(@other_user, "User message body", "User message subject")
+
+    sign_in_as!(@other_user)
+    visit messages_path
+    first(:link, "User message subject").click
+    fill_in "reply_body", with: "Here is the reply"
+    click_button "Reply"
+    click_link "Sign Out"
+
+    sign_in_as!(@user)
+    visit messages_path
+    first(:link, "User message subject").click
+    expect(page).to have_content("Here is the reply")
+    click_link "Sign Out"
   end
 end
