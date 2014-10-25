a77db5a30f27547d4a46e23c3afd93371c351545
Adam Butterfield <adb707@yahoo.com>
2014-10-24 23:17:20 -0700

    Config donation model

---
+++ app/models/donation.rb
@@ -0,0 +1,19 @@
+class Donation < ActiveRecord::Base
+  belongs_to :user
+
+  serialize :notification_params, Hash
+  def paypal_url(return_path)
+    values = {
+        business: Figaro.env.PAYPAL_BUSINESS,
+        cmd: "_xclick",
+        upload: 1,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
+        invoice: id,
+        amount: self.amount,
+        item_name: "Bonsai List Donation",
+        quantity: '1',
+        notify_url: "#{Rails.application.secrets.app_host}/hook"
+    }
+    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+end
