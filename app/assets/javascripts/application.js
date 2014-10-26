d417ae03d218ca0ba29c79e60c3227b9be4f292c
Adam Butterfield <adb707@yahoo.com>
2014-10-26 00:58:46 -0700

    Add sticky effect for marketplace nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require sticky-kit
 //= require_tree .
 
 $(function(){
 
   $(document).foundation();
+
+  $("#marketplace-nav").stick_in_parent({offset_top: 40});
 
   $(window).on("load", function () {
     var footer = $("#footer");
