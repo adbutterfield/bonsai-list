c3d22c029a77a02782cb1e823a4f45271dbc7877
Adam Butterfield <adb707@yahoo.com>
2014-10-28 15:05:04 -0700

    Add route for terms

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'faq'     => 'static_pages#faq'
+  get 'terms'   => 'static_pages#terms'
 
   root 'static_pages#home'
 
